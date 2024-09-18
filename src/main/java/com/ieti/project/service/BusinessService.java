1ad0cc94d4e89d906c706a90bcd0cb52f907d9e3
Daniel Perez <danielestebanperez22@gmail.com>
2024-09-17 19:44:38 -0500

    CrudRepository for business GET, POST, PUT

--- src/main/java/com/ieti/project/service/BusinessService.java
+++ src/main/java/com/ieti/project/service/BusinessService.java
@@ -1,6 +1,37 @@
 package com.ieti.project.service;
 
+
+import com.ieti.project.persistence.entity.BusinessEntity;
+import com.ieti.project.persistence.repository.BusinessRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BusinessService {
 
+    private final BusinessRepository businessRepository;
+
+    @Autowired
+    public BusinessService(BusinessRepository businessRepository) {
+        this.businessRepository = businessRepository;
+    }
+
+    public List<BusinessEntity> getAll(){
+        return this.businessRepository.findAll();
+    }
+
+    public BusinessEntity get(int idPizza){
+        return this.businessRepository.findById(idPizza).orElse(null);
+    }
+
+    public BusinessEntity save(BusinessEntity business){
+        return this.businessRepository.save(business);
+    }
+
+    public boolean exists(int businessId){
+        return this.businessRepository.existsById(businessId);
+    }
 
 }
---
+++ src/main/java/com/ieti/project/web/controller/BusinessController.java
@@ -0,0 +1,49 @@
+package com.ieti.project.web.controller;
+
+import com.ieti.project.persistence.entity.BusinessEntity;
+import com.ieti.project.service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/business")
+public class BusinessController {
+
+    private final BusinessService businessService;
+
+    @Autowired
+    public BusinessController(BusinessService businessService){
+        this.businessService = businessService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BusinessEntity>> getAll(){
+        return ResponseEntity.ok(this.businessService.getAll());
+    }
+
+    @GetMapping("/{businessId}")
+    public ResponseEntity<BusinessEntity> get(@PathVariable int businessId){
+        return ResponseEntity.ok(this.businessService.get(businessId));
+    }
+
+    @PostMapping
+    public ResponseEntity<BusinessEntity> add(@RequestBody BusinessEntity business){
+        if (business.getBusinessId() == null || !this.businessService.exists(business.getBusinessId())){
+            return ResponseEntity.ok(this.businessService.save(business));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+
+    @PutMapping
+    public ResponseEntity<BusinessEntity> update(@RequestBody BusinessEntity business){
+        if (business.getBusinessId() != null && this.businessService.exists(business.getBusinessId())){
+            return ResponseEntity.ok(this.businessService.save(business));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+}
