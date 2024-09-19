b4bf6146bee7e75f40a4d69d8ee204d6194d3ad5
FDanielMc <morenillodaniel440@gmail.com>
2024-09-18 21:39:43 -0500

    Creación TemplateEntity

---
+++ src/main/java/com/ieti/project/persistence/entity/TemplateEntity.java
@@ -0,0 +1,42 @@
+package com.ieti.project.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "Template")
+@Getter
+@Setter
+@NoArgsConstructor
+public class TemplateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "template_id" , nullable = false)
+    private Integer templateId;
+
+    @Column(nullable = false, length = 250)
+    private String logo;
+
+    @Column(name = "subtitle_categories", nullable = false, length = 250)
+    private String subtitleCategories;
+
+    @Column(name = "subtitle_pictures", nullable = false, length = 250)
+    private String subtitlePictures;
+
+    @Column(name = "seccion_one", nullable = false, length = 250)
+    private String seccionOne;
+
+    @Column(name = "seccion_two", nullable = false, length = 250)
+    private String seccionTwo;
+
+    @OneToOne
+    @JoinColumn(name = "business_id", referencedColumnName = "business_id", insertable = false, updatable = false)
+    private BusinessEntity businessEntity;
+
+}
