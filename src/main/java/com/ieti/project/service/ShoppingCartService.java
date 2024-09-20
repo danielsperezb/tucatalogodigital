1a6e9c865213b0a5b4ba0cd2a34c840d20052629
Daniel Perez <danielestebanperez22@gmail.com>
2024-09-19 20:27:34 -0500

    Creacion Shopping Cart Service

---
+++ src/main/java/com/ieti/project/service/ShoppingCartService.java
@@ -0,0 +1,40 @@
+package com.ieti.project.service;
+
+
+import com.ieti.project.persistence.entity.ShoppingCartEntity;
+import com.ieti.project.persistence.repository.ShoppingCartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ShoppingCartService {
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    @Autowired
+    public ShoppingCartService(ShoppingCartRepository shoppingCartRepository) {
+        this.shoppingCartRepository = shoppingCartRepository;
+    }
+
+    public List<ShoppingCartEntity> getAll(){
+        return this.shoppingCartRepository.findAll();
+    }
+
+    public ShoppingCartEntity get(int shoppingCartId){
+        return this.shoppingCartRepository.findById(shoppingCartId).orElse(null);
+    }
+
+    public ShoppingCartEntity save(ShoppingCartEntity products){
+        return this.shoppingCartRepository.save(products);
+    }
+
+    public void delete(int productsId){
+        this.shoppingCartRepository.deleteById(productsId);
+    }
+
+    public boolean exists(int productsId){
+        return this.shoppingCartRepository.existsById(productsId);
+    }
+
+
+}
